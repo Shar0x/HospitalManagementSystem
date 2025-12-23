fa088053d57fa7639eb125ad77b089483e2da93c
hilmi <hilmi@10.203.121.217>
2025-12-23 13:32:11 +0300

    added Node class

--- src/hospitalManagemenSystem/Node.java
+++ src/hospitalManagemenSystem/Node.java
@@ -1,5 +1,21 @@
 package hospitalManagemenSystem;
 
 public class Node {
+	
+	// "Object" is used so this Node can hold Strings (History) or Patients (Queue)
+	Patient patient;
+    String data; 
+    Node next;   // Reference to the next node in the list
+    
+    public Node(Patient patient) {
+    	this.patient = patient;
+        this.data = null;
+        this.next = null;
+    }
 
+    public Node(String data) {
+        this.data = data;
+        this.patient = null;
+        this.next = null;
+    }
 }
