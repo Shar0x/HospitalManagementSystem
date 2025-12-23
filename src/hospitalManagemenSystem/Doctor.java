e2f74baf06bece6d4a02e04c4e1f601c45fb0e46
Kayra Emre <Kayra Emre@10.203.64.254>
2025-12-23 13:15:27 +0300

    add doctor and patient class

--- src/hospitalManagemenSystem/Doctor.java
+++ src/hospitalManagemenSystem/Doctor.java
@@ -1,5 +1,17 @@
 package hospitalManagemenSystem;
 
 public class Doctor {
+	int id;
+	String name;
+	String department;
+	Queue waitingLine;
+
+	// Default constructor
+	public Doctor(int id, String name, String department) {
+		this.id = id;
+		this.name = name;
+		this.department = department;
+		this.waitingLine = new Queue();
+	}
 
 }
--- src/hospitalManagemenSystem/Patient.java
+++ src/hospitalManagemenSystem/Patient.java
@@ -1,5 +1,25 @@
 package hospitalManagemenSystem;
 
 public class Patient {
+	int id;
+	String name;
+	int severityLevel; 
+	LinkedList medicalHistory; 
+
+	public Patient(int id, String name, int severityLevel) {
+		this.id = id;
+		this.name = name;
+		this.severityLevel = severityLevel;
+		this.medicalHistory = new LinkedList();
+	}
+
+	public void addHistory(String record) {
+		// medicalHistory.add(record);
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + id + " | " + name + " | Seviye: " + severityLevel;
+	}
 
 }
