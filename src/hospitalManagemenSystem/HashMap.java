5161261b455de011fd46f031b5c0403b8387cfae
Kayra Emre <Kayra Emre@10.203.64.254>
2025-12-23 13:48:49 +0300

    added hashmap and hashNode classes

---
+++ src/hospitalManagemenSystem/HashMap.java
@@ -0,0 +1,60 @@
+package hospitalManagemenSystem;
+
+public class HashMap {
+	private HashNode[] table;
+	private int capacity;
+
+	public HashMap(int capacity) {
+		this.capacity = capacity;
+		table = new HashNode[capacity];
+	}
+
+	private int hash(int key) {
+		return key % capacity;
+	}
+
+	public void put(int key, Patient value) {
+		int index = hash(key);
+		HashNode newNode = new HashNode(key, value);
+
+		if (table[index] == null) {
+			table[index] = newNode;
+		} else {
+
+			HashNode current = table[index];
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = newNode;
+		}
+	}
+
+	public Patient get(int key) {
+		int index = hash(key);
+		HashNode current = table[index];
+		while (current != null) {
+			if (current.key == key)
+				return current.value;
+			current = current.next;
+		}
+		return null;
+	}
+
+	public void remove(int key) {
+		int index = hash(key);
+		HashNode current = table[index];
+		HashNode prev = null;
+
+		while (current != null) {
+			if (current.key == key) {
+				if (prev == null)
+					table[index] = current.next;
+				else
+					prev.next = current.next;
+				return;
+			}
+			prev = current;
+			current = current.next;
+		}
+	}
+}
--- src/hospitalManagemenSystem/HashNode.java
+++ src/hospitalManagemenSystem/HashNode.java
@@ -2,4 +2,13 @@ package hospitalManagemenSystem;
 
 public class HashNode {
 
+	int key;
+	Patient value;
+	HashNode next;
+
+	public HashNode(int key, Patient value) {
+		this.key = key;
+		this.value = value;
+		this.next = null;
+	}
 }
